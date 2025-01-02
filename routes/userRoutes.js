55620dd0ed4c1da86158ef38865cf6dae4ba657f
Mohd Rizwan <iamrizwan40@gmail.com>
2025-01-02 17:25:09 +0300

    restarting working on it, removed auth to redefine it

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,10 @@
 import express from "express";
-import {
-  login,
-  logout,
-  protect,
-  register,
-} from "../controllers/userController.js";
+import { login, logout, register } from "../controllers/userController.js";
 const router = express();
 
 // Register User
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/protect", protect);
 
 export default router;
