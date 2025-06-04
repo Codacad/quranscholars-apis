c20d4a9d5e3b4efc60dbe6b90f0225eee301f272
Rizwan Ansari <iamrizwan40@gmail.com>
2025-06-04 12:28:34 +0300

    changes in upload file controller

--- controllers/filesUploadContoller.js
+++ controllers/filesUploadContoller.js
@@ -21,7 +21,7 @@ export const profilePictureUpload = async (req, res) => {
 
         // 5. Get user and check for old profile picture
         const user = await User.findById(userId)
-        const oldFileName = user.profilePicture?.filename       
+        const oldFileName = user.profilePicture?.filename
 
         // Write stream
         const stream = fileRef.createWriteStream({
@@ -32,14 +32,12 @@ export const profilePictureUpload = async (req, res) => {
         stream.on('error', (err) => {
             return res.status(500).json({ error: 'Upload failed', details: err.message });
         })
-
         stream.on('finish', async () => {
             // 8. Delete old picture from Firebase if it exists
             if (oldFileName) {
                 const oldFile = bucket.file(oldFileName)
                 await bucket.delete().catch(() => { })
             }
-
             // 9. save new reference
             user.profilePicture = {
                 filename: newFileName,
@@ -47,6 +45,12 @@ export const profilePictureUpload = async (req, res) => {
             }
             await user.save()
 
+            // Signed URL
+            // const file = bucket.file(newFileName)
+            // const [signedUrl] = await file.getSignedUrl({
+            //     action: "read",
+            //     expires: Date.now() + 60 * 60 * 1000
+            // })
             // 10 Send the response
             res.status(200).send({ message: 'Profile picture updated', profilePicture: newFileName })
         })
