64de61a90248e2cf1c386047621f08220a7abbb6
Rizwan Ansari <iamrizwan40@gmail.com>
2025-06-09 12:19:48 +0300

    merge fixed

---
+++ routes/filesUploadRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { profilePictureUpload, profilePictureUrl } from "../controllers/filesUploadContoller.js";
+import multer from "multer";
+import { isAuthenticatedUser } from "../middlewares/isAuthenticated.js";
+const router = express.Router()
+const upload = multer({ storage: multer.memoryStorage() })
+router.post('/upload/profile_picture', isAuthenticatedUser, upload.single('file'), profilePictureUpload)
+router.get('/upload/profile_picture_url', isAuthenticatedUser, profilePictureUrl)
+
+export default router
